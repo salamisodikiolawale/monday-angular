837dbe390d64680f6d6666dc5cf149b532b4508c
salami <salamisodikiolawale@gmail.com>
2022-01-24 08:06:04 +0100

    add sibeBar bottom

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EspaceProjetComponent } from './espace-projet/espace-projet.component';
+import { EspaceTravailComponent } from './espace-travail/espace-travail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'espace-travail', component: EspaceTravailComponent },
+  { path: 'espace-projet', component: EspaceProjetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
